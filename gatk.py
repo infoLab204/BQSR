1b1fc58c8a52b85f44e51a6054c3b3684702ac95
infoLab204 <63629577+infoLab204@users.noreply.github.com>
2022-12-15 13:49:24 +0900

    Update gatk.py

--- gatk.py
+++ gatk.py
@@ -1,10 +1,10 @@
 import os
 
 # program setting
-BWA="/tools/bwa/"
-PICARD="/tools/picard/picard.jar"
-GATK="/tools/gatk/GenomeAnalysisTK.jar"
-SAMTOOLS="/tools/samtools/"
+BWA="~/tools/bwa/"
+PICARD="~/tools/picard/picard.jar"
+GATK="~/tools/gatk/GenomeAnalysisTK.jar"
+SAMTOOLS="~/tools/samtools/"
 
 
 # working directory 
